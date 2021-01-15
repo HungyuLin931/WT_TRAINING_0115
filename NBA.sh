86ecc92719f57cebead6070bc7ec67a0fa5ab736
Luka Lin <Luka.lin@wtmec.com>
2021-01-15 11:21:02 +0800

    lst version: add some NBA teams

--- NBA.sh
+++ NBA.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+#
+#  Program:
+#  WT_Training
+#
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo -e "Welcome to NBA !! \a \n"
+
+echo -e "=================="
+echo -e "Bulls:"
+echo -e "=================="
+echo -e "Lakers:"
+echo -e "=================="
+echo -e "Boston:"
+echo -e "=================="
 
